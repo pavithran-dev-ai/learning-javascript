35bf079a4df9ce3a0106c28d06641b3427a0ec7b
developerpavithran <developerpavithran@gmail.com>
2026-07-09 20:14:01 +0530

    Add string Practice

---
+++ strings/string-methods.js
@@ -0,0 +1,16 @@
+let str1 = "Hello, World!";
+//String methods Examples
+console.log(str1.length); // Output: 13
+console.log(str1.charAt(0)); // Output: H
+console.log(str1.indexOf("World")); // Output: 7
+console.log(str1.slice(0, 5)); // Output: Hello
+console.log(str1.toUpperCase()); // Output: HELLO, WORLD!
+console.log(str1.toLowerCase()); // Output: hello, world!
+console.log(str1.replace("World", "JavaScript")); // Output: Hello, JavaScript!
+console.log(str1.split(", ")); // Output: [ 'Hello', 'World!' ]
+console.log(str1.trim()); // Output: Hello, World!
+console.log(str1.startsWith("Hello")); // Output: true
+console.log(str1.endsWith("!")); // Output: true
+console.log(str1.includes("Hello")); // Output: true
+console.log(str1.repeat(2)); // Output: Hello, World!Hello, World!
+console.log(str1.substring(7, 12)); // Output: World
