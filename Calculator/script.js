9dc5b241cbf6768803264e668ae023caa335b5ed
developerpavithran <developerpavithran@gmail.com>
2026-08-17 18:56:49 +0530

    Three New Files are Added

---
+++ Calculator/script.js
@@ -0,0 +1,16 @@
+const display = document.getElementById('display');
+function appendToDisplay(value){
+    display.value += value;
+}
+function clearAll(){
+    display.value = '';
+}
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}function clearEntry(){
+    display.value = display.value.slice(0, -1);
+}
